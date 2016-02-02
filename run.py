67b7cf398914e9bd9137bceb9ef6ee2602fc6766
trumpet <zh19970205@126.com>
2016-02-02 17:45:00 +0000

    update

--- run.py
+++ run.py
@@ -9,8 +9,10 @@ def position():
 	if "j1" in request.args:
 		m=request.args
 		arg=["./run.m"]+map(lambda x:str(m[x]),["j1","w1","d1","j2","w2","d2"])
-		print arg
-		return subprocess.check_output(arg).replace("{","").replace("}","").replace(",","")
+		ans=subprocess.check_output(arg).replace("{","").replace("}","").replace(",","")
+		ans=ans.replace("\n","").split(" ")
+		ret="1. <a href='http://cn.bing.com/ditu/default.aspx?cp=%s~%s' target='_blank'>%sN, %sE</a><br/>2. <a href='http://cn.bing.com/ditu/default.aspx?cp=%s~%s' target='_blank'>%sN, %sE</a>"%(ans[1],ans[0],ans[1],ans[0],ans[3],ans[2],ans[3],ans[2])
+		return ret
 	else:
 		return app.send_static_file("index.html")
 
