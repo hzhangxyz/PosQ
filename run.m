3d43211fe3d5db4fbdd34980c8d63a6a32ee0d91
trumpet <zh19970205@126.com>
2016-02-03 05:50:07 +0000

    update

--- run.m
+++ run.m
@@ -1,12 +1,12 @@
 #!/usr/local/bin/WolframScript -script
-L=Read[StringToStream[#],Number]&/@$ScriptCommandLine[[2;;]]
+L=ToExpression&/@$ScriptCommandLine[[2;;]]
 Fun[p1_, d1_, p2_, d2_] := (r = 6371.4;
   s[p_] := 
    r {Cos[p[[2]]] Cos[p[[1]]], Cos[p[[2]]] Sin[p[[1]]], Sin[p[[2]]]};
   d[pp1_, pp2_] := Norm[s[pp1*Pi/180.] - s[pp2*Pi/180.]];
   {{j, w} /. 
     FindRoot[{d[p1, {j, w}] == d1, 
-      d[p2, {j, w}] == d2}, {{j, p1[[1]]}, {w, p2[[2]]}}], {j, w} /. 
+      d[p2, {j, w}] == d2}, {{j, (p1[[1]]+p2[[1]]-p2[[2]]+p1[[2]])/2}, {w, (p1[[2]]+p2[[2]]+p2[[1]]-p1[[1]])/2}}], {j, w} /. 
     FindRoot[{d[p1, {j, w}] == d1, 
-      d[p2, {j, w}] == d2}, {{j, p2[[1]]}, {w, p1[[2]]}}]})
+      d[p2, {j, w}] == d2}, {{j, (p1[[1]]+p2[[1]]+p2[[2]]-p1[[2]])/2}, {w, (p1[[2]]+p2[[2]]-p2[[1]]+p1[[1]])/2}}]})
 Print[Fun[{L[[1]],L[[2]]},L[[3]],{L[[4]],L[[5]]},L[[6]]]]
--- run.py
+++ run.py
@@ -8,7 +8,12 @@ app = Flask(__name__)
 def position():
 	if "j1" in request.args:
 		m=request.args
-		arg=["./run.m"]+map(lambda x:str(m[x]),["j1","w1","d1","j2","w2","d2"])
+		arg=map(lambda x:str(m[x]),["j1","w1","d1","j2","w2","d2"])
+		test=''.join(arg)
+		for i in test:
+			if ("a"<=i<="z") or ("A"<=i<="Z"):
+				return "Error"
+		arg=["./run.m"]+arg
 		ans=subprocess.check_output(arg).replace("{","").replace("}","").replace(",","")
 		ans=ans.replace("\n","").split(" ")
 		ret="1. <a href='http://cn.bing.com/ditu/default.aspx?cp=%s~%s' target='_blank'>%sN, %sE</a><br/>2. <a href='http://cn.bing.com/ditu/default.aspx?cp=%s~%s' target='_blank'>%sN, %sE</a>"%(ans[1],ans[0],ans[1],ans[0],ans[3],ans[2],ans[3],ans[2])
@@ -17,4 +22,4 @@ def position():
 		return app.send_static_file("index.html")
 
 if __name__ == "__main__":
-	app.run("0.0.0.0",80,debug=True)
+	app.run("0.0.0.0",80,debug=False)
